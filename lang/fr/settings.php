411e9836e9ce1e75ede7d62164ced4c964448104
Phyks <webmaster@phyks.me>
2013-10-31 10:26:51 +0100

    Completed localization for french language

---
+++ lang/fr/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * * French language file for config
+ * *
+ * */
+
+$lang['discussionPage'] = 'Page de discussion (laisser vide pour désactiver les discussions)';
+$lang['userPage'] = 'Page utilisateur (laisser vide pour désactiver les pages utilisateurs)';
+$lang['hideTools'] = 'Cacher les outils quand vous n\'êtes pas loggés ?';
+$lang['tagline'] = 'Ligne de tag (<strong>déprécié</strong>, utilise le paramètre core &quot;tagline&quot; depuis Adora Belle)';
+$lang['sidebarID'] = 'Nom de la page à utiliser pour la sidebar (<strong>déprécié</strong>, utilise le paramètre core &quot;sidebar&quot; depuis Adora Belle)';
