2706765a9a33bcf53b05ec5c0c9cde92940f6cf3
Cameron Little <cameron@camlittle.com>
2014-08-06 16:29:13 -0700

    Updated settings and added a sidebar position configuration option.
    Settings have been improved a bit, added an option to put the sidebar on
    the right side.
    
    Related to issue #31.
    Resolves issue #32.

--- conf/default.php
+++ conf/default.php
@@ -7,7 +7,6 @@
 $conf['discussionPage']   = 'discussion:@ID@';
 $conf['userPage']         = 'user:@USER@:';
 $conf['hideTools']        = 0;
-$conf['tagline']          = 'This is the tagline - explaining what this site is about.';
-$conf['sidebarID']        = 'sidebar';
 $conf['sidebar_cols']     = '3';
-$conf['google_analytics'] = 'Insert your google analytics tracking ID here';
+$conf['sidebar_pos']      = 'Left';
+$conf['google_analytics'] = '';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -7,5 +7,6 @@
 $lang['discussionPage']   = 'Discussion page (leave empty to disable discussions)';
 $lang['userPage']         = 'User page (leave empty to disable user pages)';
 $lang['hideTools']        = 'Hide tools when not logged in?';
-$lang['tagline']          = 'Tagline (<strong>deprecated</strong>, use core &quot;tagline&quot; setting since Adora Belle)';
-$lang['sidebarID']        = 'page name of page included in sidebar (<strong>deprecated</strong>, use core &quot;sidebar&quot; setting since Adora Belle)';
+$lang['sidebar_cols']     = 'Width of sidebar, 1 to 12, based off of a 12 column grid system.';
+$lang['sidebar_pos']      = 'Which side of the screen should the sidebar appear on?';
+$lang['google_analytics'] = 'Insert your google analytics tracking ID here.';
