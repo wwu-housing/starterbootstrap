4433bf7c0540814aae50a45e9a2d1c1f7d4394e3
Myeongjin <aranet100@gmail.com>
2016-04-20 00:15:29 +0900

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,12 +1,18 @@
 <?php
 /**
- * Korean language file
+ * Korean language file for template
  *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
-$lang['discussion']   = '토론';
-$lang['back_to_article'] = '위로 기사';
-$lang['userpage']       = '사용자 문서';
+$lang['discussion']        = '토론';
+$lang['back_to_article']   = '문서로 돌아가기';
+$lang['userpage']          = '사용자 문서';
 
-//Setup VIM: ex: et ts=2 :
+/* accessibility headlines (deprecated and superseded by core since Adora Belle) */
+$lang['user_tools']        = '사용자 도구';
+$lang['site_tools']        = '사이트 도구';
+$lang['page_tools']        = '문서 도구';
+$lang['skip_to_content']   = '내용으로 건너뛰기';
+$lang['skip_to_nav']       = '둘러보기로 건너뛰기';
+$lang['left']              = '왼쪽';
+$lang['right']             = '오른쪽';
---
+++ lang/ko/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Korean language file for config
+ *
+ */
+
+$lang['discussionPage']   = '토론 문서 (토론을 비활성화하려면 비워 두세요)';
+$lang['userPage']         = '사용자 문서 (사용자 문서를 비활성화하려면 비워 두세요)';
+$lang['hideTools']        = '로그인하지 않을 때 도구를 숨길까요?';
+$lang['sidebar_cols']     = '12 열 그리드 시스템의 기반으로 한 사이드바의 너비, 1부터 12까지.';
+$lang['sidebar_pos']      = '화면의 어느 쪽에 사이드바에 나타내야 합니까?';
+$lang['full_width']       = '고정 너비 단계보다는 유동 (100%) 너비 레이아웃 사용.';
+$lang['google_analytics'] = '여기에 Google 웹 로그 분석 추적 ID를 넣으세요. (비활성화하려면 비워 두세요)';
+$lang['header_img']       = '설정되어 있는 경우, 이 URL은 네비게이션 바의 브랜드 로고로 사용됩니다. 설정되어 있지 않은 경우, 테마는 도쿠위키 설치의 루트에서 `brand.png` 또는 `brand.jpg`를 찾습니다. 이미지를 여전히 찾을 수 없는 경우, 아무 것도 사용하지 않습니다.';
+$lang['header_padding']   = '헤더 이미지를 찾은 경우, 이것은 위/아래 여백으로 사용됩니다. 정수 값.';
+$lang['header_height']    = '헤더 이미지를 찾은 경우, 이것은 높이로 사용됩니다. 정수 값.';
+$lang['header_title']     = '헤더 이미지를 찾은 경우, 위키 제목도 보여줍니다.';
